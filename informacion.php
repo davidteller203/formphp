ef3a9f0e44ed1780538a509064c05970fe3f5807
davidteller203 <cr7fabianis@gmail.com>
2025-10-13 12:12:11 -0500

    Update informacion.php
    Signed-off-by: davidteller203 <cr7fabianis@gmail.com>

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
+<?php
+$nombre = $_ POST['nombre']; 
+$edad = $_POST[ ' edad ' ];
+$direccion = $_POST [' direccion' ]; 
+$salario = $_POST [' salario ' ]; 
 
+echo "<h2> informacion recibida</h2>"; 
+echo " el empleado $nombre tiene $edad años.<br>"; 
+echo "vive en $direccion y su salario es: $salario."; 
+?> 
